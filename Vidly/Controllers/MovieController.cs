9696b964c86f2e0277ac7e32ef36cf09a5fcc7c1
May Phoo Wint Zay Oo <mayphoowintzayoo@gmail.com>
2021-01-28 01:26:32 +0630

    docs : Update README files

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -13,8 +13,24 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-
             return View(movie);
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id= " + id);
+        }
+
+        public ActionResult Index(string txt)
+        {
+            return Content("Indexing ");
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "ReleaseDate" + month);
+        }
+
+
     }
 }
