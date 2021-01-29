052b3d343f121c1db69f3f4f64a4b76af4074dfd
May Phoo Wint Zay Oo <mayphoowintzayoo@gmail.com>
2021-01-30 01:40:31 +0630

    feat: Update Database

---
+++ Vidly/Migrations/202101291857548_AddDataAnnotatinsAndViewModels.cs
@@ -0,0 +1,20 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDataAnnotatinsAndViewModels : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "CustomerName", c => c.String(nullable: false));
+            AddColumn("dbo.Movies", "MovieName", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Movies", "MovieName");
+            DropColumn("dbo.Customers", "CustomerName");
+        }
+    }
+}
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Movie
     {
         public int MovieId { get; set; }
+        [Required]
+        public int MovieName { get; set; }
     }
 }
